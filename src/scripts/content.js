3d97ccbd949000a8a7c7f56c285dfd9bdfeaccbc
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-10-13 09:20:27 +0200

    perf(content-script): add conditions before inject

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -2,15 +2,28 @@ import { moveTrackList } from '../features/move-tracklist'
 import { addVolumeSlider } from '../features/add-volume-slider'
 import { copyTrackInfo } from '../features/copy-track-info'
 import { handleKeyboard } from '../features/handle-keyboard'
+import { isPageAlbum } from '../utils/is-page-album'
+import { isPageTrack } from '../utils/is-page-track'
 
 window.addEventListener ('load', () => {
 
-    moveTrackList ()
+    if (isPageAlbum ()) {
 
-    addVolumeSlider ()
+        moveTrackList ()
 
-    copyTrackInfo ()
+    }
 
-    handleKeyboard ()
+    if (
+        isPageAlbum ()
+        || isPageTrack ()
+    ) {
+
+        addVolumeSlider ()
+
+        copyTrackInfo ()
+
+        handleKeyboard ()
+
+    }
 
 })
