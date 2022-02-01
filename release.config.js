4de1c1d7619b2ea39bc18d344c7205b36230f402
Bamdad Sabbagh <hi@bamdad.fr>
2022-02-01 21:56:08 +0100

    chore(Releases): Manually add last breaking change commit to CHANGELOG.md + Remove customised preset in semantic-release config
    Removing the customised preset in semantic-release config because it has to be set at the release notes generator level too and may be more. Using `BREAKING CHANGE:` now for concerned commits instead of `breaking:`

--- release.config.js
+++ release.config.js
@@ -2,11 +2,8 @@
 
 module.exports = {
   'plugins': [
-    ['@semantic-release/commit-analyzer', {
-      preset: 'angular',
-      releaseRules: [{type: 'breaking', release: 'major'}],
-    }],
-    '@semantic-release/release-notes-generator',
+    ['@semantic-release/commit-analyzer'],
+    ['@semantic-release/release-notes-generator'],
     ['@semantic-release/changelog', {
       changelogFile: 'CHANGELOG.md',
     }],
