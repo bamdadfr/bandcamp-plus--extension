526e06e82faf7c0545fd3678cb04b90c52cd8667
Bamdad Sabbagh <hi@bamdad.fr>
2022-04-25 10:12:26 +0200

    fix(Heart): Inject dynamic fill color to ensure contrast ratio

--- src/app/components/heart.component.ts
+++ src/app/components/heart.component.ts
@@ -36,7 +36,7 @@ export class HeartComponent {
 
   // url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewbox='0 0 100 100'><path fill='%23de3618' d='M50,88.87 C76.67,70.46 90,53.9 90,39.17 C90,17.08 63.12,3.84 50,27.63 C38.875,3.85 10,17.08 10,39.17 C10,53.9 23.33,70.46 50,88.87 Z'/></svg>")
   private addLikeStyles() {
-    this.node.style.backgroundImage = 'url("data:image/svg+xml;utf8,<svg xmlns=\'http://www.w3.org/2000/svg\' width=\'100\' height=\'100\' viewbox=\'0 0 100 100\'><path fill=\'%23de3618\' stroke=\'%23' + this.colors.link_color + '\' stroke-width=\'5\' d=\'M50,88.87 C76.67,70.46 90,53.9 90,39.17 C90,17.08 63.12,3.84 50,27.63 C38.875,3.85 10,17.08 10,39.17 C10,53.9 23.33,70.46 50,88.87 Z\'/></svg>")';
+    this.node.style.backgroundImage = 'url("data:image/svg+xml;utf8,<svg xmlns=\'http://www.w3.org/2000/svg\' width=\'100\' height=\'100\' viewbox=\'0 0 100 100\'><path fill=\'%23' + this.colors.bg_color + '\' stroke=\'%23' + this.colors.link_color + '\' stroke-width=\'5\' d=\'M50,88.87 C76.67,70.46 90,53.9 90,39.17 C90,17.08 63.12,3.84 50,27.63 C38.875,3.85 10,17.08 10,39.17 C10,53.9 23.33,70.46 50,88.87 Z\'/></svg>")';
   }
 
   // url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewbox='0 0 100 100'><path fill='none' stroke='%23666' stroke-width='5' d='M50,88.87 C76.67,70.46 90,53.9 90,39.17 C90,17.08 63.12,3.84 50,27.63 C38.875,3.85 10,17.08 10,39.17 C10,53.9 23.33,70.46 50,88.87 Z'/></svg>")
