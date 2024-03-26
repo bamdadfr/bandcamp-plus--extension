acbd754535818880b6848c60dee8d8e97504da9a
Bamdad Sabbagh <hi@bamdad.fr>
2024-03-26 16:48:08 +0100

    fix(App): Migrate to v3 for chrome + Remove background script

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,6 @@ module.exports = (env) => {
     devtool: isProduction ? false : 'inline-source-map',
     entry: {
       'scripts/content': './src/app/content.ts',
-      'scripts/background': './src/app/background.ts',
     },
     resolve: {
       extensions: ['.ts', '.js'],
